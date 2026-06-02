4855783854a42a99104c0995626ba0efea0de898
sunilsisodia09 <ssisodiya9758@gmail.com>
2026-06-02 22:15:04 +0530

    Updated cart calculation function

---
+++ CalculateCart.js
@@ -0,0 +1,23 @@
+function calculateTotal(cart){
+    let subtotal = 0;
+
+    for(let item of cart){
+        subtotal += item.price * item.quantity;
+    }
+
+     let discount = 0;
+      
+    if(subtital > 50000){
+        discount = subtoatal *0.10 ;
+    }
+
+    let finalAmount = subtotal -discount;
+
+    return{
+         subtotal,
+        discount,
+        finalAmount
+    };
+       
+    
+}
