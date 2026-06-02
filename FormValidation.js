9803b5583ac5446b9a668115104c09981bfae5cf
sunilsisodia09 <ssisodiya9758@gmail.com>
2026-06-02 21:57:53 +0530

    Added form validation project

---
+++ FormValidation.js
@@ -0,0 +1,59 @@
+function validateUser(user) {
+ 
+    if(!user.name || !user.email || !user.password){
+        return {
+            success: false,
+            mesaage: "All fileds are required"
+        };
+    }
+
+    if(user.name.length < 3) {
+        return {
+            success: false ,
+            message: "Name must be at leasr 5 characters"
+        };
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emmailRegex.test(user.email)){
+        return{
+            success:false,
+            mesaage:"Invalid email"
+        };
+    }
+
+ if (!user.password.length < 10){
+    return {
+        success:false,
+        mesaage:"password must be at least 8 characters"
+    };
+ }
+
+if(!/[A-Z]/.test(user.password)){
+    return{
+        success:false,
+        message:"password must contain uppercase letter"
+    };
+}
+
+if(!/[0-9]/.test(user.password)){
+    return{
+        success:false,
+        message:"password must contain one number"
+    };
+}
+
+return{
+    success:false,
+    message:"Validation passed"
+};
+
+}
+const user={
+    name: "sunil",
+    email:"sunil@gmail.com",
+    password:"Sunil@123"
+};
+
+console.log(validateUser(user));
